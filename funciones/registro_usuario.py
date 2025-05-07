455d5e27c8a099d0cafc5ffa456193f70869d557
Camilo <camilomunozb203@gmail.com>
2025-05-06 22:11:29 -0500

    Nueva Funcion.

---
+++ funciones/registro_usuario.py
@@ -0,0 +1,56 @@
+import sqlite3
+import bcrypt
+import re
+
+class ConexionDB:
+    def __init__(self,ruta_db):
+        try:
+            self.conn = sqlite3.connect(ruta_db)
+            self.cursor = self.conn.cursor()
+        except sqlite3.Error as error:
+            print(f"Error en la base de datos: {error}.")
+    
+    def cierre_db(self):
+        self.conn.close()
+        print("Cierre de la base de datos exitoso.")
+
+class RegistroUsuario:
+    def __init__(self,conexion):
+        self.conexion = conexion
+    def nuevo_usuario(self):
+        try:
+            nombre_usuario = input("Ingresa tu nombre: ").strip()
+            apellido_usuario = input("Ingresa tu apellido de usuario: ").strip()
+            nacimiento_user = input("Ingresa tu fecha de nacimiento (dd/mm/yyyy): ").strip()
+            email_user = input("Ingresa tu correo electroico: ").strip()
+            contraseña_user = input("Crea una contraseña (maximo 6 caracteres): ").strip()
+
+            validador_contraseña = r"^[a-zA-Z0-9@#$%^&+=]{6,}$"
+            validador_email =  r"[a-zA-Z-0-9]+@[a-zA-Z]+\.[a-z-.]+$"
+            cifrado_contraseña = bcrypt.hashpw(contraseña_user.encode("utf-8"), bcrypt.gensalt())
+
+            if not all([nombre_usuario,apellido_usuario,nacimiento_user,email_user,contraseña_user]):
+                print("Todos los campos debem estar completos.")
+                return
+            elif not re.fullmatch(validador_email,email_user):
+                print("El email no cumple con los parametros, volver a intentar.")
+                return
+            elif not re.fullmatch(validador_contraseña,contraseña_user):
+                print("La contraseña no cumple con los parametros, volver a intentar.")
+                return
+            
+            self.conexion.cursor.execute("SELECT 1 FROM usuario WHERE email_user = ?",(email_user,))
+            if self.conexion.cursor.fetchone():
+                print("Este correo ya fue ingresado.")
+                return
+            
+            self.conexion.cursor.execute(
+                "INSERT INTO usuario (nombre_usuario,apellido_usuario,nacimiento_user,contraseña_user,email_user) VALUES (?,?,?,?,?)",(nombre_usuario,apellido_usuario,nacimiento_user,cifrado_contraseña,email_user))
+            
+            self.conexion.conn.commit()
+            print("Datos ingresados correctamente.")
+        
+        except sqlite3 as error:
+            print(f"Error en la base de datos: {error}.")
+        except Exception as error:
+            print(f"Error en el programa: {error}.")
--- index.py
+++ index.py
@@ -1,4 +1,8 @@
+from funciones.registro_usuario import ConexionDB, RegistroUsuario
 
+
+ruta_db = r"TU_BASE_DATOS"
+conexion = ConexionDB(ruta_db)
 
 while True:
     print("""
@@ -9,13 +13,22 @@ while True:
         3. salir
     """)
     try:
-        usuario = str(input("Ingresa la opcion que desees."))
-        if usuario == "1":
-            print("Proxima funcionalidad.")
+        usuario = str(input("Ingresa la opcion que desees: ")).strip()
+        if not usuario:
+            print("No se pueden tener campos en blanco, volver a intentar.")
+            break
+        elif usuario == "1":
+            registro = RegistroUsuario(conexion)
+            registro.nuevo_usuario()
         elif usuario == "2":
             print("Proxima funcionalidad.")
         elif usuario == "3":
             print("Gracias por preferirnos.")
             break
+        else:
+            print("Ingresar un valor valido de 1-3")
+        
+        input("\nPresiona enter para continuar...")
+
     except ValueError as error:
         print(f"Error en el programa: {error}.")
